8dbe31a0d5a17af5791be27059973816b65d1374
Hank Holmes <hholmes726@gmail.com>
2023-11-12 17:39:23 -0500

    Added basic SASS styling.

--- .cache/page-ssr/routes/component---src-pages-index-jshead.js
+++ .cache/page-ssr/routes/component---src-pages-index-jshead.js
@@ -1,4 +1,3 @@
-"use strict";
 exports.id = "component---src-pages-index-jshead";
 exports.ids = ["component---src-pages-index-jshead"];
 exports.modules = {
@@ -9,6 +8,7 @@ exports.modules = {
   \****************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   Head: () => (/* binding */ Head),
@@ -16,6 +16,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/main.scss */ "./src/styles/main.scss");
+/* harmony import */ var _styles_main_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_main_scss__WEBPACK_IMPORTED_MODULE_1__);
+
 
 var greeting = () => {
   let today = new Date();
@@ -31,17 +34,33 @@ var greeting = () => {
   }
 };
 const IndexPage = () => {
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("body", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Hank Holmes"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h2", null, "QA Analyst @ Paymentus"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, greeting(), " SaaS professional with 5+ years of quality assurance and sales experience, working with clients of all shapes & sizes across USA."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "This is my personal portfolio that I'm using to record my continuing Full Stack Web Development education. If you're interested in connecting, please shoot me an", " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("link", {
+    rel: "stylesheet",
+    href: "https://use.typekit.net/zkw6lpu.css"
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("body", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", null, "Hank Holmes"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h2", null, "QA Analyst @ Paymentus"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, greeting(), " I'm a", " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
+    href: "https://en.wikipedia.org/wiki/Software_as_a_service",
+    target: "_blank"
+  }, "SaaS"), " ", "professional with 5+ years of quality assurance and sales experience, working with clients of all shapes & sizes across the USA."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("p", null, "This is my personal, non-work portfolio that I'm using to record my continuing Full Stack Web Development education. If you're interested in connecting, please shoot me an", " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
     href: "mailto:github.unlocking468@passmail.net"
   }, "email"), " or message me on", " ", /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("a", {
     href: "https://www.linkedin.com/in/henryhamiltonholmes/",
     target: "_blank"
-  }, "LinkedIn"), " ", "and I'll be in touch!"));
+  }, "LinkedIn"), " ", "and I'll be in touch!"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("ul", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("code", null, "Email: github.unlocking468@passmail.net")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("li", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("code", null, "LinkedIn: https://www.linkedin.com/in/henryhamiltonholmes/")))));
 };
 const Head = () => {
   /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("title", null, "Hank Holmes");
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (IndexPage);
+
+/***/ }),
+
+/***/ "./src/styles/main.scss":
+/*!******************************!*\
+  !*** ./src/styles/main.scss ***!
+  \******************************/
+/***/ (() => {
+
+
 
 /***/ })
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "../styles/main.scss";
 
 var greeting = () => {
   let today = new Date();
@@ -17,45 +18,41 @@ var greeting = () => {
 
 const IndexPage = () => {
   return (
-    <body>
-      <h1>Hank Holmes</h1>
-      <h2>QA Analyst @ Paymentus</h2>
-      <p>
-        {greeting()} I'm a{" "}
-        <a
-          href="https://en.wikipedia.org/wiki/Software_as_a_service"
-          target="_blank"
-        >
-          SaaS
-        </a>{" "}
-        professional with 5+ years of quality assurance and sales experience,
-        working with clients of all shapes & sizes across the USA.
-      </p>
-      <p>
-        This is my personal, non-work portfolio that I'm using to record my
-        continuing Full Stack Web Development education. If you're interested in
-        connecting, please shoot me an{" "}
-        <a href="mailto:github.unlocking468@passmail.net">email</a> or message
-        me on{" "}
-        <a
-          href="https://www.linkedin.com/in/henryhamiltonholmes/"
-          target="_blank"
-        >
-          LinkedIn
-        </a>{" "}
-        and I'll be in touch!
-      </p>
-      <ul>
-        <li>
-          <code>Email: github.unlocking468@passmail.net</code>
-        </li>
-        <li>
-          <code>
-            LinkedIn: https://www.linkedin.com/in/henryhamiltonholmes/
-          </code>
-        </li>
-      </ul>
-    </body>
+    <>
+      <body>
+        <h1>Hank Holmes</h1>
+        <h2>QA Analyst @ Paymentus</h2>
+        <p>
+          {greeting()} I'm a{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Software_as_a_service"
+            target="_blank"
+          >
+            SaaS
+          </a>{" "}
+          professional with 5+ years of quality assurance and sales experience,
+          working with clients of all shapes & sizes across the USA.
+        </p>
+        <p>
+          This is my personal, non-work portfolio that I'm using to record my
+          continuing Full Stack Web Development education. If you're interested
+          in connecting, please shoot me an{" "}
+          <a href="mailto:github.unlocking468@passmail.net">email</a> or message
+          me on{" "}
+          <a
+            href="https://www.linkedin.com/in/henryhamiltonholmes/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>{" "}
+          and I'll be in touch!
+        </p>
+        <ul>
+          <li>Email: github.unlocking468@passmail.net</li>
+          <li>LinkedIn: https://www.linkedin.com/in/henryhamiltonholmes/</li>
+        </ul>
+      </body>
+    </>
   );
 };
 
